a1d6700d991b04666b8a691c61c197fe62910a89
frawg.lehgs@gmail.com <frawg.lehgs@gmail.com>
2019-11-18 19:48:09 -0500

    Updates

---
+++ precache-manifest.95378f7f315a128c194df464d00e58b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b764f7b40a62c72451a0f93fd365a48",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "246992ea049e4ffbfc06",
+    "url": "/robofriends/static/css/main.46a49b62.chunk.css"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/robofriends/static/js/2.49b130ef.chunk.js"
+  },
+  {
+    "revision": "246992ea049e4ffbfc06",
+    "url": "/robofriends/static/js/main.eed4b91a.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  }
+]);
